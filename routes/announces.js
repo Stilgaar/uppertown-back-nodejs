e7b157bf276f5970c0c09f2fcb58bb4c7064e6fc
Guerrier Marc-Aurele <gma06000@gmail.com>
2021-11-05 12:54:49 +0100

    modifs intégrées

--- routes/announces.js
+++ routes/announces.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const announces = require('../controllers/announces');
+const multer = require('../middleware/multer-config');
 
 router.get('/allAnnounces', announces.getAnnounces)
+router.post('/allAnnounces', multer, announces.createAnnounces)
+router.get('/:id', announces.getOneAnnounces);
+router.put('/:id', announces.modifyAnnounces);
+router.delete('/:id', announces.deleteAnnounces);
 
 
 
