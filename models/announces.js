aed1fa6f73818a2a19b8b2adae1103fa03c1cdfc
Lois Bonsignore <bonsignore.lois@gmail.com>
2021-11-09 15:45:07 +0100

    modif du model announces

--- models/announces.js
+++ models/announces.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const announceSchema = mongoose.Schema({
   title: { type: String },
   content: { type: String }, 
+  bedrooms: { type: String },
+  surface: { type: String },
   city: { type: String },
   price: { type: Number },
   share_price: { type: Number },
